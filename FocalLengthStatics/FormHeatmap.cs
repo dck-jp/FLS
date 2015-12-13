7c2daa7f102ae27b76ade64bd9db390d08e0e76f
dck-jp <dicek.am@clockahead.com>
2015-12-13 22:46:43 +0900

    fix problem , renew ZMappingData.dll ; 生成されるheatmapのピクセル数が少ない場合に、縦方向の補完がうまくいかず、pictureboxの表示がおかしくなる(下部に透明のスペースができる)問題を解消

--- FocalLengthStatics/FormHeatmap.cs
+++ FocalLengthStatics/FormHeatmap.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -95,14 +96,65 @@ namespace FocalLengthStatics
             var w = pictureBox1.Width;
             var h = pictureBox1.Height;
 
-            var canvas = new Bitmap(w, h);
+            var canvas = new Bitmap(w, image.Height);
             var g = Graphics.FromImage(canvas);
             g.InterpolationMode = InterpolationMode.NearestNeighbor;
-            g.DrawImage(image, 0, 0, w, h);
+            g.DrawImage(image, 0, 0, w, image.Height);
             g.Dispose();
+
+            if (image.Height < h)
+            {
+                canvas = InflateVertical(canvas, h);
+            }
 
             pictureBox1.Image = canvas;
         }
+
+        /// <summary>
+        /// 縦方向に膨らませる
+        /// </summary>
+        /// <param name="oldCanvas"></param>
+        /// <param name="requiredHeight">必要高さ;pictureboxのサイズ</param>
+        /// <returns></returns>
+        private Bitmap InflateVertical(Bitmap oldCanvas, int requiredHeight)
+        {
+            //縦方向の決定 requiredHeightを越えるように
+            var multiple = (requiredHeight / oldCanvas.Height) + 1;
+            var newHeight = oldCanvas.Height * multiple;
+            var newCanvas = new Bitmap(oldCanvas.Width, newHeight);
+
+            var newCanvasPlus = new BitmapPlus.BitmapPlus(newCanvas);
+            var oldCanvasPlus = new BitmapPlus.BitmapPlus(oldCanvas);
+            newCanvasPlus.BeginAccess();
+            oldCanvasPlus.BeginAccess();
+            // 拡大処理
+            var w = oldCanvas.Width;
+
+            Enumerable.Range(0, oldCanvas.Height)
+                      .ToList()
+                      .ForEach(i =>
+                     {
+                         for (var j = 0; j < w; j++)
+                         {
+                             var c = oldCanvasPlus.GetPixel(j, i);
+                             for (var k = 0; k < multiple; k++)
+                             {
+                                 newCanvasPlus.SetPixel(j, i * multiple + k, c);
+                             }
+                         }
+                     });
+
+            //**********
+            newCanvasPlus.EndAccess();
+            oldCanvasPlus.EndAccess();
+            return newCanvas;
+        }
+
+        internal Rectangle Rect(Bitmap canvas)
+        {
+            return new Rectangle(0, 0, canvas.Width, canvas.Height);
+        }
+
 
         internal void SetTitle(string title)
         {
